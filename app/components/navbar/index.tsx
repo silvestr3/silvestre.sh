24bf8ff4560fa0ff8b8a9509ca28850d627cc315
silvestr3 <fellipe.silvestre@outlook.com>
2026-01-31 14:58:28 -0300

    feat: :sparkles: Add Separator component and integrate into Navbar and Home page

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -21,20 +21,22 @@ export function Navbar() {
   ];
 
   return (
-    <header className="w-[min(100%,1024px)] mx-auto py-6 px-4 flex justify-between items-center bg-background fixed top-0 left-0 right-0">
-      <div className="flex items-baseline gap-1">
-        <span className="font-bold text-xl tracking-tight">fs</span>
-        <div className="rounded-full w-1 h-1 bg-primary" />
+    <header className="w-full bg-background fixed top-0 left-0 right-0 z-10">
+      <div className="w-[min(100%,1024px)] mx-auto py-6 px-4 flex justify-between items-center">
+        <div className="flex items-baseline gap-1">
+          <span className="font-bold text-xl tracking-tight">fs</span>
+          <div className="rounded-full w-1 h-1 bg-primary" />
+        </div>
+        <nav>
+          <ul className="flex gap-6">
+            {navItems.map((item, index) => (
+              <li key={index}>
+                <UnderlineLink href={item.href} label={item.label} />
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul className="flex gap-6">
-          {navItems.map((item, index) => (
-            <li key={index}>
-              <UnderlineLink href={item.href} label={item.label} />
-            </li>
-          ))}
-        </ul>
-      </nav>
     </header>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Hero } from "@/app/components/hero";
 import { Navbar } from "./components/navbar";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Separator />
     </>
   );
 }
