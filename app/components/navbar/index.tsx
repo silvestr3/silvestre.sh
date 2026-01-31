1cb6febcfab147296d08ece3f730358c0fa81fb7
silvestr3 <fellipe.silvestre@outlook.com>
2026-01-31 16:52:01 -0300

    style: :art: Update Navbar styles for improved alignment and font size

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -22,9 +22,9 @@ export function Navbar() {
 
   return (
     <header className="w-full bg-background fixed top-0 left-0 right-0 z-10">
-      <div className="w-[min(100%,1024px)] mx-auto py-6 px-4 flex justify-between items-center">
+      <div className="w-[min(100%,1024px)] mx-auto py-6 px-4 flex justify-between items-baseline">
         <div className="flex items-baseline gap-1">
-          <span className="font-bold text-xl tracking-tight">fs</span>
+          <span className="font-bold text-2xl tracking-tight">fs</span>
           <div className="rounded-full w-1 h-1 bg-primary" />
         </div>
         <nav>
