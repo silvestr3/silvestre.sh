32449f76e9a6ea9d8f4c3c80b9ff7ad35644c6a3
silvestr3 <fellipe.silvestre@outlook.com>
2026-01-31 14:00:52 -0300

    feat: :sparkles: Add Illustration component and integrate into Hero section

--- app/components/hero/index.tsx
+++ app/components/hero/index.tsx
@@ -1,37 +1,43 @@
 import { Button } from "@/components/ui/button";
 import FlipWords from "@/components/ui/flip-words";
 import { LinkRow } from "./components/link-rows";
+import { Illustration } from "./components/illustration";
 
 export function Hero() {
   return (
-    <section className="flex flex-col min-h-screen items-left p-4 px-4 md:px-12 lg:px-16 justify-center font-sans bg-background gap-10">
-      <div className="flex flex-col gap-2">
-        <span className="uppercase text-primary font-semibold">
-          fellipe silvestre
-        </span>
-        <h1 className="text-foreground text-5xl font-bold flex flex-wrap items-center gap-3">
-          Desenvolvedor
-          <FlipWords
-            duration={3000}
-            className="text-inherit p-0 m-0"
-            words={[
-              "Front end",
-              "Javascript",
-              "Typescript",
-              "React",
-              "React Native",
-              "Node",
-              " ",
-            ]}
-          />
-        </h1>
-        <h2 className="text-muted text-lg">Da interface ao runtime</h2>
+    <section className="flex-col-reverse flex lg:flex-row items-center lg:justify-between min-h-screen w-[min(100%,1600px)] mx-auto items-left p-4 px-4 md:px-12 lg:px-16 font-sans bg-background gap-10">
+      <div className="flex flex-col gap-10">
+        <div className="flex flex-col gap-2">
+          <span className="uppercase text-primary font-semibold">
+            fellipe silvestre
+          </span>
+          <h1 className="text-foreground text-5xl font-bold flex flex-wrap items-center gap-3">
+            Desenvolvedor
+            <FlipWords
+              duration={3000}
+              className="text-inherit p-0 m-0"
+              words={[
+                "Front end",
+                "Javascript",
+                "Typescript",
+                "React",
+                "React Native",
+                "Node",
+              ]}
+            />
+          </h1>
+          <h2 className="text-muted text-lg">Da interface à runtime</h2>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Button className="mr-6 rounded-lg">Ver projetos</Button>
+
+          <LinkRow />
+        </div>
       </div>
 
-      <div className="flex items-center gap-2">
-        <Button className="mr-6 rounded-lg">Ver projetos</Button>
-
-        <LinkRow />
+      <div className="w-148 max-w-full">
+        <Illustration />
       </div>
     </section>
   );
