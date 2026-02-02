c13cf3017c63bd4170cd1b63c7da8c70afe66444
silvestr3 <fellipe.silvestre@outlook.com>
2026-02-01 21:10:41 -0300

    fix: :bug: Wrap logo in Link component for proper navigation

--- components/global/navbar/index.tsx
+++ components/global/navbar/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { UnderlineLink } from "./components/underline-link";
 
 export function Navbar() {
@@ -23,10 +24,12 @@ export function Navbar() {
   return (
     <header className="w-full bg-background fixed top-0 left-0 right-0 z-10">
       <div className="w-[min(100%,1024px)] mx-auto py-6 px-4 flex justify-between items-baseline">
-        <div className="flex items-baseline gap-1">
-          <span className="font-bold text-2xl tracking-tight">fs</span>
-          <div className="rounded-full w-1 h-1 bg-primary" />
-        </div>
+        <Link href={"/"}>
+          <div className="flex items-baseline gap-1">
+            <span className="font-bold text-2xl tracking-tight">fs</span>
+            <div className="rounded-full w-1 h-1 bg-primary" />
+          </div>
+        </Link>
         <nav>
           <ul className="flex gap-6">
             {navItems.map((item, index) => (
